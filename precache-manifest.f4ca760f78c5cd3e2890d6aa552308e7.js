6f686e0d1e3a9fee93a87413a8bdfcaf29e4f361
Jasmine Wang <jasminey.tea@gmail.com>
2020-04-22 13:09:45 -0500

    Updates

---
+++ precache-manifest.f4ca760f78c5cd3e2890d6aa552308e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce55ddde35fe973023037acbf5f2201",
+    "url": "/noisemaker/index.html"
+  },
+  {
+    "revision": "6668d062928a26d9e756",
+    "url": "/noisemaker/static/css/main.13ec3d40.chunk.css"
+  },
+  {
+    "revision": "2ecb3f2f0ac8d3bf13e8",
+    "url": "/noisemaker/static/js/2.dc9d0437.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/noisemaker/static/js/2.dc9d0437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6668d062928a26d9e756",
+    "url": "/noisemaker/static/js/main.8febadb4.chunk.js"
+  },
+  {
+    "revision": "2c4e78b5762c4cf9af5c",
+    "url": "/noisemaker/static/js/runtime-main.8fa71543.js"
+  }
+]);
