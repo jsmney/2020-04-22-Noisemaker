c94ebfc041e7de211a0dd20a864781ab55c3b119
Jasmine Wang <jasminey.tea@gmail.com>
2020-04-22 12:54:20 -0500

    added some fun random sounds by button

--- src/App.js
+++ src/App.js
@@ -1,68 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 import * as Tone from 'tone'
 
 function App() {
   //create a synth and connect it to the master output (your speakers)
-  const synth = new Tone.Synth().toMaster()
+  const [synthPreset, setSynthPreset] = useState({
+    on: false,
+    adsr: [0, 0, 0, 0]
+  })
+  const synth = synthPreset.on
+    ? new Tone.Synth({
+        oscillator: {
+          type: 'pwm',
+          modulationFrequency: 0.2
+        },
+        envelope: {
+          attack: synthPreset.adsr[0],
+          decay: synthPreset.adsr[1],
+          sustain: synthPreset.adsr[2],
+          release: synthPreset.adsr[3]
+        }
+      }).toMaster()
+    : new Tone.Synth().toMaster()
 
   const handleClick = (evt) => {
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('C4', '8n')
+    //play note (determined by evt.target) for the duration of an 8th note
+    synth.triggerAttackRelease(evt.target.value, '8n')
   }
 
   return (
     <div className="App">
       <div>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
+        <button onClick={handleClick} value="C6"></button>
+        <button onClick={handleClick} value="D6"></button>
+        <button onClick={handleClick} value="E6"></button>
+        <button onClick={handleClick} value="F6"></button>
+        <button onClick={handleClick} value="G6"></button>
+        <button onClick={handleClick} value="A6"></button>
+        <button onClick={handleClick} value="B6"></button>
       </div>
       <div>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
+        <button onClick={handleClick} value="C5"></button>
+        <button onClick={handleClick} value="D5"></button>
+        <button onClick={handleClick} value="E5"></button>
+        <button onClick={handleClick} value="F5"></button>
+        <button onClick={handleClick} value="G5"></button>
+        <button onClick={handleClick} value="A5"></button>
+        <button onClick={handleClick} value="B5"></button>
       </div>
       <div>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
+        <button onClick={handleClick} value="C4"></button>
+        <button onClick={handleClick} value="D4"></button>
+        <button onClick={handleClick} value="E4"></button>
+        <button onClick={handleClick} value="F4"></button>
+        <button onClick={handleClick} value="G4"></button>
+        <button onClick={handleClick} value="A4"></button>
+        <button onClick={handleClick} value="B4"></button>
       </div>
       <div>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
+        <button onClick={handleClick} value="C3"></button>
+        <button onClick={handleClick} value="D3"></button>
+        <button onClick={handleClick} value="E3"></button>
+        <button onClick={handleClick} value="F3"></button>
+        <button onClick={handleClick} value="G3"></button>
+        <button onClick={handleClick} value="A3"></button>
+        <button onClick={handleClick} value="B3"></button>
       </div>
       <div>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
-        <button onClick={handleClick}></button>
+        <button onClick={handleClick} value="C2"></button>
+        <button onClick={handleClick} value="D2"></button>
+        <button onClick={handleClick} value="E2"></button>
+        <button onClick={handleClick} value="F2"></button>
+        <button onClick={handleClick} value="G2"></button>
+        <button onClick={handleClick} value="A2"></button>
+        <button onClick={handleClick} value="B2"></button>
+      </div>
+      <div>
+        <button
+          id="synth"
+          onClick={() =>
+            setSynthPreset({
+              on: !synthPreset.on,
+              adsr: [Math.random(), Math.random(), Math.random(), Math.random()]
+            })
+          }
+        >
+          {synthPreset ? 'Back to vanilla sound' : 'Change that sound!'}
+        </button>
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>Make some noise!</h1>
+    <p>
+      Hackathon using <a href="https://github.com/Tonejs/Tone.js/">Tone.js</a>{' '}
+      made by <a href="http://jasminey.com/">@jsmney</a> for{' '}
+      <a href="https://github.com/MintbeanHackathons/2020-04-22-Noisemaker">
+        Mintbean Hackathons
+      </a>
+    </p>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
